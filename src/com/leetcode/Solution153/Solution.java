e83f329ed5678b4041d19f22fb23488733fe3e16
machao24 <machao24@meituan.com>
2019-11-20 19:56:46 +0800

    add 153 solution

--- src/com/leetcode/Solution153/Solution.java
+++ src/com/leetcode/Solution153/Solution.java
@@ -20,23 +20,23 @@ package com.leetcode.Solution153;
 public class Solution {
     public int findMin(int[] nums) {
         int left = 0;
-        int right = nums.length;
-        if (right == 1) {
-            return nums[0];
-        }
+        int right = nums.length - 1;
+
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[left]) {
-                left = mid;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
             }
             else {
                 right = mid;
             }
         }
-        return Math.min(nums[left], nums[right]);
+        return nums[right];
     }
 
     public static void main(String[] args) {
-
+        Solution s = new Solution();
+        int testArr[] = {3,4,5,1,2};
+        System.out.println(s.findMin(testArr));
     }
 }
