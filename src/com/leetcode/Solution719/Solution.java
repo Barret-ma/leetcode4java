5e383902ce121ef7462305332ca8254da0d65e55
machao24 <machao24@meituan.com>
2019-11-21 20:15:18 +0800

    add 719 solution

--- src/com/leetcode/Solution719/Solution.java
+++ src/com/leetcode/Solution719/Solution.java
@@ -1,5 +1,7 @@
 package com.leetcode.Solution719;
 
+import java.util.Arrays;
+
 //Given an integer array, return the k-th smallest distance among all the pairs. The distance of a pair (A, B) is defined as the absolute difference between A and B.
 //
 //        Example 1:
@@ -20,6 +22,27 @@ package com.leetcode.Solution719;
 
 public class Solution {
     public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
 
+        int n = nums.length;
+        int left = 0;
+        int right = nums[n - 1] - nums[0];
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int cnt = 0, start = 0;
+            for (int i = 0; i < n; i ++) {
+                while (start < n && nums[i] - nums[start] > mid) start ++;
+                cnt += i - start;
+            }
+            if (cnt < k) left = mid + 1;
+            else right = mid;
+        }
+        return right;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int testArr[] = {1,3,1};
+        System.out.println(s.smallestDistancePair(testArr, 1));
     }
 }
